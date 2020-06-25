5fd650c636d82fd23aecb62cb67fa8ffec6ec81e
drewsef-916 <drewm541@gmail.com>
2020-06-25 15:30:44 -0700

    initial commit

---
+++ src/components/BuildingIndex.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <ul>
+      {{ sortBuildings.buildingname }}
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import buildingJson from '@/assets/buildingData.json'
+
+export default {
+  name: 'BuildingIndex',
+  data: function() {
+    return {
+      buildings: buildingJson.data.items,
+      sortedBuildings: [],
+      buildingsByZone: []
+    }
+  },
+  methods: {
+    // getBuildingData: function() {
+    //   axios.get('https://applefacilities.review.blueriver.com/index.cfm/_api/json/v1/scv/building/?andOpenGrouping&locationCode%5B0%5D=sqo&or&locationCode%5B2%5D=nwr&or&locationCode%5B4%5D=scv&or&locationCode%5B6%5D=sfo&closeGrouping&fields=buildingname,buildingabbr,lat,lng,black,buildingZone&active=1&cachedwithin=600')
+    //   .then(res => this.buildings = res.data.items)
+    //   .catch(err => console.log(err));
+    // },
+    // getBuildingJson: function() {
+    //   this.buildings = buildingJson.data.items
+    // },
+    sortByBuildingZone: function() {
+      // const zoneList = [];
+      // const zoneGroups = [];
+      // console.log(sortedBuildings);
+      // this.buildings.forEach(building => {
+      //   if (!zoneList.includes(building.buildingzone)) zoneList.push(building.buildingzone)
+      // });
+
+      // this.buildingZones = zoneList;
+    }
+  },
+  computed: {
+    sortBuildings() {
+      return this.buildings.sort((a,b) => a.buildingname - b.buildingname);
+    }
+  },
+  created() {
+    this.sortByBuildingZone();
+  }
+}
+</script>
+
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Index</h1>
+    <BuildingIndex />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import BuildingIndex from '@/components/BuildingIndex.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    BuildingIndex
   }
 }
 </script>
+
+<style lang="scss">
+
+</style>
